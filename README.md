da9b6bcddb2f5e4230b45e463215d7409ad30a82
Michael Harrison <harrim91@hotmail.co.uk>
2018-02-26 00:18:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Virtual Pet
+
+Virtual Pet solution - intro to OOP/TDD in JavaScript
+
+Pet has a name
+Pet can get older, which makes it hungrier and less fit
+Pet can be fed to decrease hunger
+Pet can be walked to increase fitness
+Pet can inform you how it's feeling
+If Pet's hunger reaches 10, it will die
+If Pet's fitness reaches 0, it will die
+If Pet reaches age 30, it will die
+If you try to interact with a dead Pet, it will throw an error
+
+## Setup
+
+- clone this repo
+- `npm install`
+
+## Running tests
+Unit tests use jest - `npm test`
+
+
---
+++ __tests__/pet.test.js
@@ -0,0 +1,47 @@
+const Pet = require('../src/pet');
+
+describe('constructor', () => {
+  it('returns an object', () => {
+    expect(new Pet()).toBeInstanceOf(Object);
+  });
+
+  it('sets the name property', () => {
+    const pet = new Pet('Fido');
+    expect(pet.name).toEqual('Fido');
+  });
+
+  it('has a initial age of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.age).toEqual(0);
+  });
+
+  it('has a initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+    expect(pet.hunger).toEqual(0);
+  });
+
+  it('has a initial fitness of 10', () => {
+    const pet = new Pet('Fido');
+    expect(pet.fitness).toEqual(10);
+  });
+});
+
+describe('growUp', () => {
+  it('increments the age by 1', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.age).toEqual(1);
+  });
+
+  it('increments hunger by 5', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.hunger).toEqual(5);
+  });
+
+  it('decreases fitness by 3', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+    expect(pet.fitness).toEqual(7);
+  });
+});
